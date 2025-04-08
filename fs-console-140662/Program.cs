e114f64fa426e78ff359e1c09ad7f357a8a1f825
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-04-08 20:42:50 +0100

    Movie

--- fs-console-140662/Program.cs
+++ fs-console-140662/Program.cs
@@ -3,5 +3,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to Full Stack");
 Console.WriteLine("Welcome from Carrickmines, this is your host calling");
 
+Console.WriteLine("Black Bag was the last movie I went to");
+
 
 
