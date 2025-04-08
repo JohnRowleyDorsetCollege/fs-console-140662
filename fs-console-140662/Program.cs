57eebd3435d0ab111075ed2c304fd8bf9a5a8a7f
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-04-08 20:39:33 +0100

    Added a greeting

--- fs-console-140662/Program.cs
+++ fs-console-140662/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to Full Stack");
+Console.WriteLine("Welcome from Carrickmines, this is your host calling");
 
 
 
